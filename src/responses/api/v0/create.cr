875ca71c0c06098a0cd03f9100eee1a8e54f7871
deing <admin@15318.de>
2019-04-07 17:17:44 +0200

    Update API version to v1, add docs for resolve

--- src/responses/api/v0/create.cr
+++ src/responses/api/v0/create.cr
@@ -2,11 +2,18 @@ require "../../../logic"
 
 post "/api/v0/create" do |env|
   env.response.content_type = "application/json"
+  env.response.status_code = 403
+  {status: "error"}.to_json
+end
+
+post "/api/v1/create" do |env|
+  env.response.content_type = "application/json"
   url = env.request.body.not_nil!.gets_to_end
   begin
     hash = SHAShortLogic.create_link(url)
     next {status: "success", hash: hash}.to_json
   rescue e
+    puts e.to_s
     next case e
     when SHAShortLogic::RateLimitError
       env.response.status_code = 429
--- src/responses/api/v0/resolve.cr
+++ src/responses/api/v0/resolve.cr
@@ -1,13 +1,14 @@
 require "../../../logic"
 
-get "/api/v0/resolve" do |env|
+get "/api/v1/resolve" do |env|
   env.response.content_type = "application/json"
-  hash = env.request.body.not_nil!.gets_to_end
+  hash = URI.unescape(env.params.query["hash"])
+  hash = hash.lchop("https://ding.15318.de/resolve/").strip
   begin
     url = SHAShortLogic.fetch_link(hash)
     next {status: "success", url: url}.to_json
   rescue e
-		puts e
+		puts e.to_s
     next case e
     when SHAShortLogic::InvalidHashLengthError
       env.response.status_code = 400
