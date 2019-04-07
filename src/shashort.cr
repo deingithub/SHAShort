2def00a4722b0ee232b1490f12426583db8f19d6
deing <admin@15318.de>
2019-04-07 17:43:49 +0200

    Allow site to work nicely without JS

--- src/shashort.cr
+++ src/shashort.cr
@@ -35,6 +35,9 @@ after_all do |env|
   env.response.headers.add("Content-Security-Policy", "default-src 'none'; img-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; connect-src 'self'")
   env.response.headers.add("Strict-Transport-Security", "max-age=63072000")
   env.response.headers.add("X-Frame-Options", "DENY")
+  unless env.request.path.includes?("/api/v")
+    env.response.headers.add("Content-Type", "text/html; charset=utf-8")
+  end
 end
 
 Dotenv.load!
